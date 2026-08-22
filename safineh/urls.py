d68fcdc3e969e4e68587d1bdabe8cab99253655c
Abolfazlnikomi <shahiabolfazlhak840@gmail.com>
2026-08-22 13:30:32 +0330

    Add files via upload

--- safineh/urls.py
+++ safineh/urls.py
@@ -1,22 +1,33 @@
-"""
-URL configuration for safineh project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.shortcuts import redirect
+from django.urls import include, path
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+
+    # مسیرهای CKEditor برای آپلود تصاویر داخل متن خبر
+    path("ckeditor/", include("ckeditor_uploader.urls")),
+
+    # هدایت صفحه اصلی سایت به صفحه اخبار
+    path(
+        "",
+        lambda request: redirect("news_modal:article_list"),
+    ),
+
+    # آدرس‌های اپ اخبار
+    path(
+        "news/",
+        include("news_modal.urls"),
+    ),
 ]
+
+
+# نمایش فایل‌های آپلودی فقط در حالت توسعه
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT,
+    )
