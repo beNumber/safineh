09ee547fc97f7bce597d9dd81591b92bed545048
Abolfazlnikomi <shahiabolfazlhak840@gmail.com>
2026-08-22 16:16:35 +0330

    safine and news code

---
+++ news_modal/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class NewsModalConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "news_modal"
+    verbose_name = "مدیریت اخبار"
